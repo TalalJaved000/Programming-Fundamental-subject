439025439c3f7ca013e58e0b0441d7c7d8a1655c
TalalJaved000 <talaljaveed001@gmail.com>
2026-02-21 18:49:43 +0500

    second week tasks

---
+++ Horse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    cout << "            ---------------------------------------                                                          "<<endl;                                                                       
+    cout << "                o    ^__^                                                         "<<endl;                                                                                          
+    cout << "                                                                      "<<endl;                                                                                             
+    cout << "                 o   (oo)\\_________                                                          "<<endl;                                                                                                                                                                                          
+    cout << "                     (__)\\         )\\/\\                                                        "<<endl; 
+    cout << "                         ||------w   |                                                     "<<endl;
+    cout << "                                                                              "<<endl;
+    cout << "                         ||         ||                                                    "<<endl;
+}
+   
